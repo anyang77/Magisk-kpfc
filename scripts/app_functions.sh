988c114e2f10fca10096faecae949cf3b8a76252
Magisk Dev <magisk@example.com>
2026-02-03 13:59:30 +0800

    22

--- scripts/app_functions.sh
+++ scripts/app_functions.sh
@@ -20,8 +20,8 @@ env_check() {
   if [ "$2" -ge 25210 ]; then
     [ -b "$MAGISKTMP/.magisk/device/preinit" ] || [ -b "$MAGISKTMP/.magisk/block/preinit" ] || return 2
   fi
-  grep -xqF "MAGISK_VER='$1'" "$MAGISKBIN/util_functions.sh" || return 3
-  grep -xqF "MAGISK_VER_CODE=$2" "$MAGISKBIN/util_functions.sh" || return 3
+  # Skip version string check to support prebuilt binaries with different versions
+  # Only check version code (30600) in Java (EnvFixDialog.kt)
   return 0
 }
 
